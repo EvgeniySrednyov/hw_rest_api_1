f6e0e946a0d93f75ee0bc45c63afd04b025762ff
EvgeniySrednev <srednevei89@gmail.com>
2024-05-14 08:18:57 +0700

    fix tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,10 @@
 import requests
 from jsonschema import validate
 
-from shemas.create_user_shema import create_user
-from shemas.list_users_shema import list_users
-from shemas.update_user_shema import update_user
+from schemas.single_resource_schema import single_resource
+from schemas.create_user_schema import create_user
+from schemas.list_users_schema import list_users
+from schemas.update_user_schema import update_user
 
 URL = 'https://reqres.in/api'
 
@@ -13,6 +14,7 @@ payload = {"name": name, "job": job}
 
 def test_get_users_list():
     response = requests.get(url=f'{URL}/users', params={"page": 2})
+
     body = response.json()
     count_users = [element["id"] for element in body["data"]]
 
@@ -22,6 +24,7 @@ def test_get_users_list():
 
 def test_create_user():
     response = requests.post(url=f'{URL}/users', data=payload)
+
     body = response.json()
 
     validate(body, schema=create_user)
@@ -32,6 +35,7 @@ def test_create_user():
 
 def test_update_user():
     response = requests.put(url=f'{URL}/users/10', data=payload)
+
     body = response.json()
 
     validate(body, schema=update_user)
@@ -44,9 +48,10 @@ def test_delete_non_existent_user():
     response = requests.delete(url=f'{URL}/users/999')
 
     assert response.status_code == 204
+    assert response.text == ''
 
 
-def test_resourse_not_found():
+def test_resource_not_found():
     response = requests.get(url=f'{URL}/unknown/999')
 
     assert response.status_code == 404
@@ -67,4 +72,15 @@ def test_generating_correct_email():
     user_email = f'{first_name.lower()}.{last_name.lower()}@reqres.in'
 
     assert body['data']['email'] == user_email
+
+
+def test_get_single_resource():
+    response = requests.get(url=f'{URL}/unknown/2')
+
+    body = response.json()
+
+    validate(body, schema=single_resource)
+    assert response.status_code == 200
+    assert body['data']['pantone_value'] == '17-2031'
+
 
